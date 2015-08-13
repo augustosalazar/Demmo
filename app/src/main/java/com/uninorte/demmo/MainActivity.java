d4b9f808f83ac9fd766a10d3bb3162c1eed8f40c
Augusto Salazar <augustosalazar@uninorte.edu.co>
2015-08-13 10:41:36 -0500

    init

--- app/src/main/java/com/uninorte/demmo/MainActivity.java
+++ app/src/main/java/com/uninorte/demmo/MainActivity.java
@@ -9,9 +9,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button b1;
+    private Button b1,b2,b3,b4;
     private elClick c;
 
     @Override
@@ -19,9 +22,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 = (Button) findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.button1);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
 
         //b1.setOnClickListener(this);
+
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        for (int i = 1; i <= 4; ++i) number.add(i);
+        Collections.shuffle(number);
+
+        b1.setText("" + number.get(0));
+        b2.setText("" + number.get(1) );
+        b3.setText("" + number.get(2) );
+        b4.setText("" + number.get(3) );
     }
 
     @Override
@@ -48,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.button){
+        if(v.getId() == R.id.button1){
 
         }
 
